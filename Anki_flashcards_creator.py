a6f029998abac1a9cd0f59a32e618e9bb86c70b4
jdazz <Jean.demael@gmail.com>
2024-10-11 21:53:16 +0200

    update openai API access

--- Anki_flashcards_creator.py
+++ Anki_flashcards_creator.py
@@ -1,10 +1,11 @@
 # import required packages
 import PyPDF2
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key='YOUR-API-KEY')
 import os
 
 # Initialize OpenAI API with your key
-openai.api_key = 'YOUR-API-KEY'
 
 ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
@@ -38,21 +39,19 @@ def create_anki_cards(pdf_text,):
     # text = divided_sections[0]
     generated_flashcards = ' '
     for i, text in enumerate(divided_sections):
-    
+
         ## You might need to change the Prompt to get consistent format.
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": f"Create anki flashcards with the provided text using a format: question;answer next line question;answer etc. Keep question and the corresponding answer on the same line {text}"}
             ]
 
-        response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages, 
-                temperature =0.3,
-                max_tokens=2048
-            )
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=messages, 
+        temperature =0.3,
+        max_tokens=2048)
 
-        response_from_api = response['choices'][0]['message']['content']#.strip()
+        response_from_api = response.choices[0].message.content#.strip()
         generated_flashcards += response_from_api
 
         if i==0:
@@ -67,7 +66,7 @@ def create_anki_cards(pdf_text,):
 if __name__ == "__main__":
 
     pdf_text = read_pdf(f'{ROOT_DIRECTORY}/SOURCE_DOCUMENTS/constitution.pdf')
-    
+
     create_anki_cards(pdf_text)
 
 
